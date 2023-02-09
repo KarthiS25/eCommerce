d9a129577aa0503bb8eeb17393ec9671db0b1141
karthi <karthis1015@gmail.com>
2023-02-09 15:17:19 +0530

    bootstrap install

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,10 @@
 class Admin::ProductsController < ApplicationController
+    before_action :authenticate_user!
     def index
+        @products = get_product_data
+
+        respond_to do |format|
+            format.html
+        end
     end
 end
